9d081c9132fc77897ad8856cb6c1201f51ca7344
RenofFahri <Renoffahriprasetyo@gmail.com>
2022-03-08 22:31:02 +0700

    Add files via upload

---
+++ set.php
@@ -0,0 +1,14 @@
+<?php
+# [ BY : Jii-Chan ]
+# [ YT : Ryuk Xan ]
+# Version : v0.10
+
+//masukan username akun tumbal
+$username="lagi_service"; 
+
+//masukan password akun tumbal
+$pswd="renof25@";
+
+//masukan username ig yang akan
+//di tambah follow
+$your_ig="renoffp";
